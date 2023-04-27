77fa9ced945996b18902600bd95203e602e88fd9
Antonio Victor <avps2@aluno.ifal.edu.br>
2023-04-26 23:28:46 -0300

    mapa foi add
    mapa funcionando

--- src/TelaPrincipal.js
+++ src/TelaPrincipal.js
@@ -1,3 +1,4 @@
+import MapView from 'react-native-maps';
 import {Text, TextInput, View, Image, Pressable, Linking }   from 'react-native';
 
 import styles from '../Styles/StyleTelaLogin';
@@ -5,10 +6,17 @@ import styles from '../Styles/StyleTelaLogin';
 
 function TelaPrincipal({ navigation }) {
   return (
-    <View style={styles.container}>
-<Text>Ola Antonio</Text>
-<View id='map' style={{height:100}}></View>
-<View>{}</View>
+      <View>
+        <Text style={{position:'relative', fontSize:20, color:'#fff', backgroundColor:'#000', }}>Ola antonio</Text>
+<MapView style={{height:1000,}}
+  initialRegion={{
+      latitude: -9.511852,
+      longitude:  -35.804028,
+      latitudeDelta: 0.0622,
+      longitudeDelta: 0.01921,
+    }}
+/>
+
     </View>
 
   );
