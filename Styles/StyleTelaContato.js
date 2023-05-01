5732e6745e9758ceadf1161a9c41354c5c26037b
Antonio <avps2@aluno.ifal.edu.br>
2023-05-01 16:08:10 -0300

    Organizando...

--- Styles/StyleTelaContato.js
+++ Styles/StyleTelaContato.js
@@ -1,108 +1,112 @@
 import { StyleSheet } from 'react-native';
 
- const stylesTelaLogin = StyleSheet.create({
-   
-    telamain:{
+const stylesTelaLogin = StyleSheet.create({
+
+    telamain: {
         backgroundColor: '#113036',
-    
-        flex:1,
-       alignItems:'center',
-    },
-   
-   
-    container:{
-     flex:1,
-     justifyContent:'center',
-    textAlign: 'center',
-    justifyItems:'center' ,
-    alignItems:'center',
 
-     backgroundColor: '#113036',
-
-   },
-  
-    titulo:{
-    marginTop:70,
-    
-    fontSize:32,
-    fontWeight:700,
-    
-    color:'#fff',
-    
-  },
-
-    subTitulo:{
-        marginTop:-120,
-       
-        fontSize:24,
-        fontWeight:700,
-        
-        color:'#fff',
-        
-    },
-    dev:{
-        marginTop:80,
-   
-       alignItems:'center',
-
-       gap:16,
-    },
-    
-    contatoNome:{
-        fontSize:20,
-        fontWeight:700,
-        color:'#fff',
-    },
-    contatoCidade:{
-        fontSize:20,
-        fontWeight:700,
-        color:'#fff',
-    },
-    contatoFone:{
-        fontSize:20,
-        fontWeight:700,
-        color:'#fff',
-    },
-    contatoIcon:{
-        fontSize:18,
-        fontWeight:700,
-        color:'#fff',
+        flex: 1,
+        alignItems: 'center',
     },
 
-    icons:{
-        flexDirection:'row',
-        gap:15,
-        justifyContent:'space-between',
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        justifyItems: 'center',
+        alignItems: 'center',
+
+        backgroundColor: '#113036',
 
     },
-    icones:{
-        fontSize:28,
 
-        color:'#fff',
-    },
-    linkInscricao:{
-      color:'#fff',
-      bottom:0,
-    },
-    linkTexto:{
-      fontWeight:600,
-      textAlign:'center',
-      color:'#aaa',
+    titulo: {
+        marginTop: 70,
+
+        fontSize: 32,
+        fontWeight: 700,
+
+        color: '#fff',
 
     },
-    linkTextoSenha:{
-      color:'#605C5d',
-      textAlign:'center',
-      fontWeight:700,
+
+    subTitulo: {
+        marginTop: 20,
+
+        fontSize: 24,
+        fontWeight: 700,
+
+        color: '#fff',
 
     },
-    linkTextoInscrevar:{
-      color:'#fff',
-      fontWeight:700,
-      marginLeft:10,
-      paddingLeft:10,
-    
+    dev: {
+        marginTop: 80,
+
+        alignItems: 'center',
+
+        gap: 16,
     },
- })
+
+    contatoNome: {
+        fontSize: 20,
+        fontWeight: 700,
+        color: '#fff',
+    },
+    contatoCidade: {
+        fontSize: 18,
+        fontWeight: 700,
+        color: '#fff',
+    },
+    contatoFone: {
+        fontSize: 18,
+        fontWeight: 700,
+        color: '#fff',
+    },
+    contatoEmail: {
+        fontSize: 16,
+        fontWeight: 700,
+        color: '#fff',
+    },
+    contatoIcon: {
+        fontSize: 20,
+        color: '#fff',
+    },
+
+    icons: {
+        flexDirection: 'row',
+        gap: 15,
+        justifyContent: 'space-between',
+
+    },
+    icones: {
+        fontSize: 28,
+
+        color: '#fff',
+    },
+    linkInscricao: {
+        color: '#fff',
+        bottom: 0,
+    },
+    linkTexto: {
+        fontWeight: 600,
+        textAlign: 'center',
+        color: '#aaa',
+
+    },
+    linkTextoSenha: {
+        color: '#605C5d',
+        textAlign: 'center',
+        fontWeight: 700,
+
+    },
+    linkTextoInscrevar: {
+        color: '#fff',
+        fontWeight: 700,
+        marginLeft: 10,
+        paddingLeft: 10,
+
+    },
+})
 
 export default stylesTelaLogin;
--- src/TelaContato.js
+++ src/TelaContato.js
@@ -8,102 +8,114 @@ function TelaContato() {
     return (
         <View style={styles.telamain}>
             <Text style={styles.titulo}>Contatos</Text>
-        <View style={styles.container}>
-            <Text style={styles.subTitulo}>Desenvolvedores</Text>
-           
-            <View style={styles.dev}>
-            <Text style={styles.contatoNome}>Antonio Victor Pereira Severiano</Text>
-            <Text style={styles.contatoCidade}>Maceió / AL</Text>
-            <Text style={styles.contatoFone}><Icon name="phone" style={styles.contatoIcon} /> Tel: (82) 99198-1626</Text>
-            <View style={styles.icons}>
-           
-            <IconButton 
-                icon="whatsapp"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://api.whatsapp.com/message/O4I654ATQMPYE1?autoload=1&app_absent=0');}}
-            />
-            <IconButton 
-                icon="email"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://chat.google.com/room/AAAAC1GfjrU?cls=1');}}
-            />
-            <IconButton 
-                icon="linkedin"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://www.linkedin.com/in/antonio-victor-pereira-severiano-0aa170169/');}}
-            />
-            <IconButton 
-                icon="instagram"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://www.instagram.com/antoniovictor2k');}}
-            />
-            <IconButton
-                icon="github"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://github.com/antoniovictor2k');}}
-            />
-            </View>
-            </View>
+            <View style={styles.container}>
+                <Text style={styles.subTitulo}>Desenvolvedores</Text>
 
-            <View style={styles.dev}>
+                <View style={styles.dev}>
+                    <Text style={styles.contatoNome}>Antonio Victor Pereira Severiano</Text>
+                    <Text style={styles.contatoCidade}>Maceió / AL</Text>
+                    <Text style={styles.contatoEmail}><Icon name="email" style={styles.contatoIcon} /> avps2@aluno.ifal.edu.br</Text>
+                    <Text style={styles.contatoFone}><Icon name="phone" style={styles.contatoIcon} /> Tel: (82) 99198-1626</Text>
+                    <View style={styles.icons}>
 
-        <Text style={styles.contatoNome}>Jeallyson Ferreira da Silva</Text>
-        <Text style={styles.contatoCidade}>Rio Largo / AL</Text>
-        <Text style={styles.contatoFone}><Icon name="phone" style={styles.contatoIcon} /> Tel: (82) 99999-9999</Text>
-        <View style={styles.icons}>
-      
-        <IconButton 
-                icon="whatsapp"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://github.com/jeallyson');}}
-            />
-            <IconButton 
-                icon="email"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://chat.google.com/room/AAAAC1GfjrU?cls=1');}}
-            />
-            <IconButton 
-                icon="linkedin"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://github.com/jeallyson');}}
-            />
-            <IconButton 
-                icon="instagram"
-                iconColor={'#fff'}
-                size={26}
-                onPress={() => {
-                    Linking.openURL('https://www.instagram.com/jeallyson_ferreira');}}
-            />
-            <IconButton
-                icon="github"
-                iconColor={'#fff'}
-                size={26}       
-                onPress={() => {
-                    Linking.openURL('https://github.com/jeallyson');}}
-            />
+                        <IconButton
+                            icon="whatsapp"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://api.whatsapp.com/message/O4I654ATQMPYE1?autoload=1&app_absent=0');
+                            }}
+                        />
+                        <IconButton
+                            icon="email"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://chat.google.com/room/AAAAC1GfjrU?cls=1');
+                            }}
+                        />
+                        <IconButton
+                            icon="linkedin"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://www.linkedin.com/in/antonio-victor-pereira-severiano-0aa170169/');
+                            }}
+                        />
+                        <IconButton
+                            icon="instagram"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://www.instagram.com/antoniovictor2k');
+                            }}
+                        />
+                        <IconButton
+                            icon="github"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://github.com/antoniovictor2k');
+                            }}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.dev}>
+
+                    <Text style={styles.contatoNome}>Jeallyson Ferreira da Silva</Text>
+                    <Text style={styles.contatoCidade}>Rio Largo / AL</Text>
+                    <Text style={styles.contatoEmail}><Icon name="email" style={styles.contatoIcon} /> jfs63@aluno.ifal.edu.br</Text>
+                    <Text style={styles.contatoFone}><Icon name="phone" style={styles.contatoIcon} /> Tel: (82) 99999-9999</Text>
+                    <View style={styles.icons}>
+
+                        <IconButton
+                            icon="whatsapp"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://github.com/jeallyson');
+                            }}
+                        />
+                        <IconButton
+                            icon="email"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://chat.google.com/room/AAAAC1GfjrU?cls=1');
+                            }}
+                        />
+                        <IconButton
+                            icon="linkedin"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://github.com/jeallyson');
+                            }}
+                        />
+                        <IconButton
+                            icon="instagram"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://www.instagram.com/jeallyson_ferreira');
+                            }}
+                        />
+                        <IconButton
+                            icon="github"
+                            iconColor={'#fff'}
+                            size={26}
+                            onPress={() => {
+                                Linking.openURL('https://github.com/jeallyson');
+                            }}
+                        />
+
+                    </View>
+
+
+                </View>
 
             </View>
-            
-
-            </View>
-        
-        </View>
         </View>
     );
 };
