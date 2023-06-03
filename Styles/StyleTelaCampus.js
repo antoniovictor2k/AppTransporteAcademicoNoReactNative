230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
Antonio <avps2@aluno.ifal.edu.br>
2023-06-03 00:49:40 -0300

    att

--- Styles/StyleTelaCampus.js
+++ Styles/StyleTelaCampus.js
@@ -2,29 +2,42 @@ import { StyleSheet, StatusBar } from 'react-native';
 
 const stylesTelaCampus = StyleSheet.create({
 
-    main:{
+    main: {
 
         flex: 1,
         backgroundColor: '#113036',
     },
     container: {
-        marginTop: StatusBar.currentHeight || 0,
 
-        textAlign: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        justifyItems: 'center',
+        marginTop: 20,
+        // textAlign: 'center',
+        // justifyContent: 'center',
+        // textAlign: 'center',
+        // justifyItems: 'center',
         alignItems: 'center',
+        // marginBottom:20,
+    },
+    list: {
+
+         alignItems: 'center',
+        backgroundColor: '#051C21',
+        margin: 1,
+        borderRadius: 10,
+        padding: 6,
+        width: 380,
     },
     titulo: {
-        marginTop: 100,
+        marginTop: 30,
         // marginBottom: 40,
         fontSize: 32,
         fontWeight: 700,
         color: '#fff',
         alignItems: 'center',
         textAlign: 'center',
+    },
+    listTexto: {
 
+        textAlign: 'left',
 
     },
     sobTitulo: {
@@ -48,6 +61,13 @@ const stylesTelaCampus = StyleSheet.create({
 
 
     },
+    imgIFAL:{
+        
+        justifyContent:'center',
+        width: 250,
+        height: 150,
+        resizeMode: 'contain',
+    },
     imgLogo: {
         marginTop: 45,
         width: 145,
@@ -70,19 +90,13 @@ const stylesTelaCampus = StyleSheet.create({
         paddingTop: 6,
         textAlign: 'center',
     },
+
     
-    list: {
-        backgroundColor: '#101010',
-        margin: 1,
-        borderRadius: 10,
-        paddingLeft: 6,
-        width:380,
-      },
     developer: {
         marginTop: 50,
-        marginBottom: 0,
-        color:'#fff',
-        textAlign:'center',
+        marginBottom: 20,
+        color: '#93B8C2',
+        textAlign: 'center',
 
     },
 
--- src/TelaCampus.js
+++ src/TelaCampus.js
@@ -1,126 +1,129 @@
-import { IconButton, ActivityIndicator, Button, TextInput, List, Avatar, } from 'react-native-paper';
-import { View, Text, FlatList, SafeAreaView, Image, ScrollView } from 'react-native';
+import React from 'react';
+import { IconButton, ActivityIndicator, Button, TextInput, List, Avatar, Card } from 'react-native-paper';
+import { View, Text, FlatList, SafeAreaView, Image, ScrollView, } from 'react-native';
 import styles from '../Styles/StyleTelaCampus';
 
 function RenderizarCampus() {
 
   const campi = [
     {
+      id:1,
       nome: 'Benedito Bentes',
       anoFundacao: 2016,
       diretor: 'Alexandre Bonfim',
       // img: require(''),
     },
     {
+      id:2,
       nome: 'Poço',
       anoFundacao: 1909,
       diretor: 'Damião Augusto',
       // img: require(''),
     },
     {
+      id:3,
       nome: 'Marechal deodoro',
       anoFundacao: 1995,
       diretor: 'Éder Souza.',
       // img: require(''),
     },
-    // {
-    //   nome: 'Satuba',
-    //   anoFundacao: 1911,
-    //   diretor: 'Uilliane Faustino',
-    //   // img: require(''),
-    // },
-    // {
-    //   nome: 'Rio Largo',
-    //   anoFundacao: 2014,
-    //   diretor: 'Edel Alexandre',
-    //   // img: require(''),
-    // },
-    // {
-    //   nome: 'Viçosa',
-    //   anoFundacao: 2016,
-    //   diretor: 'Valdomiro Odilon',
-    //   // img: require(''),
-    // },
-    // {
-    //   nome: 'Viçosa',
-    //   anoFundacao: 2016,
-    //   diretor: 'Valdomiro Odilon',
-    //   // img: require(''),
-    // },
-    // {
-    //   nome: 'Viçosa',
-    //   anoFundacao: 2016,
-    //   diretor: 'Valdomiro Odilon',
-    //   // img: require(''),
-    // },
-    // {
-    //   nome: 'Viçosa',
-    //   anoFundacao: 2016,
-    //   diretor: 'Valdomiro Odilon',
-    //   // img: require(''),
-    // },
-    // {
-    //   nome: 'Viçosa',
-    //   anoFundacao: 2016,
-    //   diretor: 'Valdomiro Odilon',
-    //   // img: require(''),
-    // },
+    {
+      id:4,
+      nome: 'Satuba',
+      anoFundacao: 1911,
+      diretor: 'Uilliane Faustino',
+      // img: require(''),
+    },
+    {
+      id:5,
+      nome: 'Rio Largo',
+      anoFundacao: 2014,
+      diretor: 'Edel Alexandre',
+      // img: require(''),
+    },
+    {
+      id:6,
+      nome: 'Viçosa',
+      anoFundacao: 2016,
+      diretor: 'Valdomiro Odilon',
+      // img: require(''),
+    },
+    {
+      id:7,
+      nome: 'Viçosa',
+      anoFundacao: 2016,
+      diretor: 'Valdomiro Odilon',
+      // img: require(''),
+    },
+    {
+      id:8,
+      nome: 'Viçosa 8',
+      anoFundacao: 2016,
+      diretor: 'Valdomiro Odilon',
+      // img: require(''),
+    },
+    {
+      id:9,
+      nome: 'Viçosa',
+      anoFundacao: 2016,
+      diretor: 'Valdomiro Odilon',
+      // img: require(''),
+    },
+    {
+      id:10,
+      nome: 'Nao sei',
+      anoFundacao: 2016,
+      diretor: 'Valdomiro Odilon',
+      // img: require(''),
+    },
   ];
 
-
-  return (
-    <SafeAreaView style={styles.container}>
-    <FlatList
-      data={campi}
-      renderItem={({ item }) => (
-        <View>
-          <List.Item
-            style={styles.list}
-            title={() => (
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          {campi.map((item) => (
+            <View key={item.id} style={styles.list}>
+              {/* <Avatar.Image size={60} source={item.img} /> */}
+              <Image
+        style={styles.imgIFAL}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+              {/* <View style={styles.listTexto}> */}
               <Text style={styles.sobTitulo}>
-                IFAL - Campus:{' '}
-                <Text style={styles.sobTitulo}>{item.nome}</Text>
+                Campus: <Text style={styles.sobTitulo}>{item.nome}</Text>
               </Text>
-            )}
-            description={() => (
-              <View>
                 <Text style={styles.sobTitulo}>
-                  Inaugurado em:{' '}
-                  <Text style={styles.sobTitulo}>
-                    {item.anoFundacao}
-                  </Text>
+                  Logitudade:{' '}
+                  <Text style={styles.sobTitulo}>{item.anoFundacao}</Text>
                 </Text>
                 <Text style={styles.sobTitulo}>
-                  Diretor(a):{' '}
-                  <Text style={styles.sobTitulo}>{item.diretor}</Text>
+                  Latitude: <Text style={styles.sobTitulo}>{item.diretor}</Text>
                 </Text>
-              </View>
-            )}
-            left={(props) => <Avatar.Image size={50} source={item.img} />}
-          />
-        </View>
-      )}
-    />
-      <Text style={styles.developer}>Developer by JEVA System - 2023</Text>
-  </SafeAreaView>
-);
-
-};
-
-function TelaCampus(){
-return(
-  <View style={styles.main}>
-  <View style={styles.container}>
-    <Text style={styles.titulo}>Antonio</Text>
-<SafeAreaView>
-    <RenderizarCampus />
-</SafeAreaView>
-  </View>
-    <Text style={styles.developer}>Developer by JEVA System - 2023</Text>
-
-</View>
-)
-
-};
+              {/* </View> */}
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+  
+  function TelaCampus() {
+    return (
+      <ScrollView style={styles.main}>
+       
+          <Text style={styles.titulo}>Campus - IFAL</Text>
+         
+            <RenderizarCampus />
+          
+        
+        <Text style={styles.developer}>Developer by JEVA System - 2023</Text>
+      </ScrollView>
+    );
+  }
+  
+ 
+  
 
 export default TelaCampus;
