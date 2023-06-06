8d7b0c05d418fbb402172830226131950f85e3b6
Antonio <avps2@aluno.ifal.edu.br>
2023-06-06 20:25:18 -0300

    Corrigindo Conflito

--- Styles/StyleTelaCampus.js
+++ Styles/StyleTelaCampus.js
@@ -3,15 +3,10 @@ import { StyleSheet, StatusBar } from 'react-native';
 const stylesTelaCampus = StyleSheet.create({
 
     main: {
-<<<<<<< HEAD
-=======
-
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
         flex: 1,
         backgroundColor: '#113036',
     },
     container: {
-<<<<<<< HEAD
         marginTop: StatusBar.currentHeight || 0,
         textAlign: 'center',
         flex:1,
@@ -19,25 +14,6 @@ const stylesTelaCampus = StyleSheet.create({
         justifyItems: 'center',
         justifyContent:'center',
         alignItems:'center',
-=======
-
-        marginTop: 20,
-        // textAlign: 'center',
-        // justifyContent: 'center',
-        // textAlign: 'center',
-        // justifyItems: 'center',
-        alignItems: 'center',
-        // marginBottom:20,
-    },
-    list: {
-
-         alignItems: 'center',
-        backgroundColor: '#051C21',
-        margin: 1,
-        borderRadius: 10,
-        padding: 6,
-        width: 380,
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
     },
     titulo: {
         marginTop: 30,
@@ -47,14 +23,6 @@ const stylesTelaCampus = StyleSheet.create({
         color: '#fff',
         alignItems: 'center',
         textAlign: 'center',
-<<<<<<< HEAD
-=======
-    },
-    listTexto: {
-
-        textAlign: 'left',
-
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
     },
     instituto: {
         fontSize: 20,
@@ -62,52 +30,12 @@ const stylesTelaCampus = StyleSheet.create({
         color: '#fff',
         textAlign: 'center',
     },
-<<<<<<< HEAD
     localizacao: {
         fontSize: 16,
-=======
-    textoPadrao: {
-        marginTop: 16,
-        color: "#cccccc",
-        fontWeight: 500,
-        fontSize: 18,
-        width: 330,
-        alignItems: 'center',
-        textAlign: 'justify',
-
-
-    },
-    imgIFAL:{
-        
-        justifyContent:'center',
-        width: 250,
-        height: 150,
-        resizeMode: 'contain',
-    },
-    imgLogo: {
-        marginTop: 45,
-        width: 145,
-        height: 80,
-    },
-
-    button: {
-        width: 350,
-        height: 50,
-        color: '#fff',
-        backgroundColor: '#000',
-        borderRadius: 10,
-        marginBottom: 0,
-        marginTop: 5,
-    },
-    textoButton: {
-        color: '#fff',
-        fontSize: 25,
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
         fontWeight: 700,
         color: '#fff',
         textAlign: 'center',
     },
-<<<<<<< HEAD
     resLocalizacao: {
         fontSize: 16,
         fontWeight: 700,
@@ -133,10 +61,6 @@ const stylesTelaCampus = StyleSheet.create({
     icones: {
         flexDirection:'row'
     },
-=======
-
-    
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
     developer: {
         marginTop: 50,
         marginBottom: 20,
--- src/TelaCampus.js
+++ src/TelaCampus.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import { IconButton, ActivityIndicator, Button, TextInput, List, Avatar, } from 'react-native-paper';
-=======
-import React from 'react';
-import { IconButton, ActivityIndicator, Button, TextInput, List, Avatar, Card } from 'react-native-paper';
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
 import { View, Text, FlatList, SafeAreaView, Image, ScrollView, } from 'react-native';
 import styles from '../Styles/StyleTelaCampus';
 
@@ -11,55 +6,33 @@ function RenderizarCampus() {
 
   const campi = [
     {
-<<<<<<< HEAD
       instituto: 'Arapiraca',
       longitude: 4534534,
       latitude: 44784345,
-=======
-      id:1,
-      nome: 'Benedito Bentes',
-      anoFundacao: 2016,
-      diretor: 'Alexandre Bonfim',
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
       // img: require(''),
       linkInstagram: '',
       linkSiteOficial: '',
       linkGoogleMaps: '',
     },
     {
-<<<<<<< HEAD
       instituto: 'Benedito Bentes',
       longitude: 4534534,
       latitude: 44784345,
-=======
-      id:2,
-      nome: 'Poço',
-      anoFundacao: 1909,
-      diretor: 'Damião Augusto',
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
       // img: require(''),
       linkInstagram: '',
       linkSiteOficial: '',
       linkGoogleMaps: '',
     },
     {
-<<<<<<< HEAD
       instituto: 'Benedito Bentes',
       longitude: 4534534,
       latitude: 44784345,
-=======
-      id:3,
-      nome: 'Marechal deodoro',
-      anoFundacao: 1995,
-      diretor: 'Éder Souza.',
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
       // img: require(''),
       linkInstagram: '',
       linkSiteOficial: '',
       linkGoogleMaps: '',
     },
     {
-<<<<<<< HEAD
       instituto: 'Benedito Bentes',
       longitude: 4534534,
       latitude: 44784345,
@@ -238,104 +211,5 @@ function TelaCampus() {
     </View>
   );
 }
-=======
-      id:4,
-      nome: 'Satuba',
-      anoFundacao: 1911,
-      diretor: 'Uilliane Faustino',
-      // img: require(''),
-    },
-    {
-      id:5,
-      nome: 'Rio Largo',
-      anoFundacao: 2014,
-      diretor: 'Edel Alexandre',
-      // img: require(''),
-    },
-    {
-      id:6,
-      nome: 'Viçosa',
-      anoFundacao: 2016,
-      diretor: 'Valdomiro Odilon',
-      // img: require(''),
-    },
-    {
-      id:7,
-      nome: 'Viçosa',
-      anoFundacao: 2016,
-      diretor: 'Valdomiro Odilon',
-      // img: require(''),
-    },
-    {
-      id:8,
-      nome: 'Viçosa 8',
-      anoFundacao: 2016,
-      diretor: 'Valdomiro Odilon',
-      // img: require(''),
-    },
-    {
-      id:9,
-      nome: 'Viçosa',
-      anoFundacao: 2016,
-      diretor: 'Valdomiro Odilon',
-      // img: require(''),
-    },
-    {
-      id:10,
-      nome: 'Nao sei',
-      anoFundacao: 2016,
-      diretor: 'Valdomiro Odilon',
-      // img: require(''),
-    },
-  ];
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <ScrollView>
-          {campi.map((item) => (
-            <View key={item.id} style={styles.list}>
-              {/* <Avatar.Image size={60} source={item.img} /> */}
-              <Image
-        style={styles.imgIFAL}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
-              {/* <View style={styles.listTexto}> */}
-              <Text style={styles.sobTitulo}>
-                Campus: <Text style={styles.sobTitulo}>{item.nome}</Text>
-              </Text>
-                <Text style={styles.sobTitulo}>
-                  Logitudade:{' '}
-                  <Text style={styles.sobTitulo}>{item.anoFundacao}</Text>
-                </Text>
-                <Text style={styles.sobTitulo}>
-                  Latitude: <Text style={styles.sobTitulo}>{item.diretor}</Text>
-                </Text>
-              {/* </View> */}
-            </View>
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    );
-  }
-  
-  function TelaCampus() {
-    return (
-      <ScrollView style={styles.main}>
-       
-          <Text style={styles.titulo}>Campus - IFAL</Text>
-         
-            <RenderizarCampus />
-          
-        
-        <Text style={styles.developer}>Developer by JEVA System - 2023</Text>
-      </ScrollView>
-    );
-  }
-  
- 
-  
->>>>>>> 230df8b170d8ea2e8d9d1f6a1f8c3b5aa1a8cdb4
 
 export default TelaCampus;
